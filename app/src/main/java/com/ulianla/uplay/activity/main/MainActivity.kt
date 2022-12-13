afbbaeb0f42e88207126690c639c3eea155f7f28
Chen <yyg@ygink.com>
2022-12-14 00:27:27 +0800

    优化布局，优化色彩搭配

--- app/src/main/java/com/ulianla/uplay/activity/main/MainActivity.kt
+++ app/src/main/java/com/ulianla/uplay/activity/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.ulianla.uplay.R
 import com.ulianla.uplay.databinding.ActivityMainBinding
@@ -20,7 +21,11 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        navView.setupWithNavController(navController)
+        /*
+            https://blog.csdn.net/qq_40785165/article/details/113522182
+            FragmentContainerView 与 NavHostFragment
+         */
+        val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navView.setupWithNavController(navController.navController)
     }
 }

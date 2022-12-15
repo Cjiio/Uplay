36e4f540ac145d342f9583e956c31bcec0ee335e
Chen <yyg@ygink.com>
2022-12-16 04:34:53 +0800

    服务器列表超过屏幕时，向下滑动隐藏创建服务器按钮

--- app/src/main/java/com/ulianla/uplay/ui/servers/ServersFragment.kt
+++ app/src/main/java/com/ulianla/uplay/ui/servers/ServersFragment.kt
@@ -1,15 +1,20 @@
 package com.ulianla.uplay.ui.servers
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.ulianla.uplay.databinding.FragmentServersBinding
 
 class ServersFragment : Fragment() {
 
     private var _binding: FragmentServersBinding? = null
+    private var NSView:NestedScrollView? = null
+    private var extended_fab: ExtendedFloatingActionButton? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -21,8 +26,17 @@ class ServersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentServersBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+        NSView = binding.NSView
+        extended_fab = binding.extendedFab
+        //NestedScrollVie向下滑动时隐藏FAB 向上滑动时显示FAB
+        NSView?.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            if (scrollY - oldScrollY > 0) {
+                extended_fab?.hide()
+            } else {
+                extended_fab?.show()
+            }
+        }
+        return binding.root
     }
 
     override fun onDestroyView() {
